b0ee308f05427b5f4b29f07b92413bd619be4b87
SOLGOR1 <info.sorgol@proton.me>
2024-07-20 19:22:55 +0200

    V1.4

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,9 +1,14 @@
-// api/webhook.js
+// pages/api/webhook.js
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-      console.log('Received webhook:', req.body); // Zum Debuggen
+      // Hier kannst du die Daten verarbeiten
+      console.log('Received webhook:', req.body);
+  
+      // Sende eine erfolgreiche Antwort zurück
       res.status(200).json({ status: 'success' });
     } else {
-      res.status(405).json({ error: 'Method Not Allowed' });
+      // Wenn nicht POST, dann 405 Fehler
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   }
