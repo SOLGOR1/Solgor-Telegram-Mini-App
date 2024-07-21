0ea59bd1f630775a3c80e46f1ae1d0f9af847cc5
SOLGOR1 <info.sorgol@proton.me>
2024-07-21 02:15:08 +0200

    jo2

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Stelle sicher, dass das Element mit der ID 'root' vorhanden ist
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-// Erstelle und rendere die React-Anwendung
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
